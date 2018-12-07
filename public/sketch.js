09094df091e7e5ecb83472310cd494565ce207d8
SaajanM <saajan.maslanka@gmail.com>
2018-12-06 21:14:32 -0700

    New Stuff

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,47 @@
+let userName;
+let w;
+let h;
+let phrase;
+function preload() {
+    userName = window.prompt("Full Name");
+    w = displayWidth;
+    h = displayHeight;
+    phrase = "Full Screen\nand New Question";
+}
+function windowResized() {
+    w = displayWidth;
+    h = displayHeight;
+    resizeCanvas(w / 1.01, h / 1.05);
+}
 function setup() {
-    createCanvas(256, 256);
+    createCanvas(w / 1.01, h / 1.05);
     background(200);
+    rectMode(CENTER);
+}
+function touchEnded() {
+    fullscreen();
+    phrase = "New Question";
+    loadJSON("/public/getq", propmtQuestion);
 }
 function draw() {
-    //Executed Every Frame
+    background(200);
+    translate(width / 2, height / 2);
+    strokeWeight((width + height) / (2 * 10));
+    stroke(0);
+    rect(0, 0, width, height);
+    textSize((width + height) / (2 * 15));
+    strokeWeight(0);
+    textAlign(CENTER);
+    text("Click For \n"+phrase, 0, 0);
+}
+function promptQuestion(json) {
+    let qArray = json.data;
+    let question = qArray[0];
+    let answer = qArray[1];
+    let userAns = window.prompt(question);
+    if (userAns.toLowerCase() == answer.toLowerCase()) {
+        //Correct
+    } else {
+        //Incorrect
+    }
 }
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 let express = require('express');
+let ndjson = require('ndjson');
 let app = express();
 app.use(express.static("./"));
-
 app.listen(8080);
+
+let questions = [];
+fs.createReadStream('rainbow.ndjson')
+    .pipe(ndjson.parse())
+    .on('data', function (obj) {
+        questions.push(obj);
+    });
+let index;
+app.get('/public/getq', (request, response) => {
+    index = Math.floor(Math.random() * questions.length);
+    response.send(questions[index]);
+});
